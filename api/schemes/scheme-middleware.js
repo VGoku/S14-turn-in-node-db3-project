4ba8dea25f504a8e090842bb8a3fd5b3709c5e45
David S. <neojohn2312@gmail.com>
2024-11-12 14:21:32 -0600

    validateScheme-working

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -18,6 +18,8 @@ const checkSchemeId = async (req, res, next) => {
       next({ status:404,
         message: `scheme with scheme_id ${req.params.scheme_id} not found`,
       })
+    } else {
+      next()
     }
   } catch (err) {
     next(err)
@@ -33,7 +35,16 @@ const checkSchemeId = async (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
+  const { scheme_name } = req.body;
+  if (
+    scheme_name === undefined ||
+    typeof scheme_name !== "string" ||
+    !scheme_name.trim()
+  ) {
+    next({ status: 400, message: "invalid scheme_name" })
+  } else {
+    next()
+  }
 }
 
 /*
